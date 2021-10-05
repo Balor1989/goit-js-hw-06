f944862519122be04faafa424994af042633d962
Evgeniy Grishko <evgeniygrish@gmail.com>
2021-10-05 16:11:26 +0300

    Выполняет 2-ю задачу

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+// Create an elements list
+const ingredientsList = document.createElement('ul') 
+
+// Create a list of element properties
+for (const ingredient of ingredients) {
+  const item = document.createElement('li');
+  item.textContent = ingredient;
+  item.classList.add('item')
+
+ingredientsList.append(item)
+}
+// Output to the console our elements list
+console.log(ingredientsList)
