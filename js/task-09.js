03357259a64291acebf17401c1fa26f24df6ad30
Evgeniy Grishko <evgeniygrish@gmail.com>
2021-10-06 23:09:45 +0300

    Добавляет решение 9-й задачи
    Требуется рефакторинг кода

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
+const styleOfBody = document.querySelector('body')
+const changeColorBtn = document.querySelector('.change-color')
+const backgroundColorValue = document.querySelector('.color')
+
+changeColorBtn.addEventListener('click', getRandomHexColor)
+
 function getRandomHexColor() {
+  backgroundColorValue.textContent = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  styleOfBody.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
