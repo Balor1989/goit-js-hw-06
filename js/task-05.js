8a3ff8d316bb8ab416fd9c362e0ef4dcaec91843
Evgeniy Grishko <evgeniygrish@gmail.com>
2021-10-06 16:12:06 +0300

    Выполняет рефакторинг кода в 5-й задаче

--- js/task-05.js
+++ js/task-05.js
@@ -3,8 +3,8 @@ const nameInputEl = document.querySelector('#name-input');
 const nameOutputEl = document.querySelector('#name-output')
 
 // Изменяет значение заголовка в соответствии с введенными данными
-nameInputEl.addEventListener('input', (event) => {
-nameOutputEl.textContent = event.currentTarget.value;
-})
-
-
+nameInputEl.addEventListener('input', (symbol =>
+    symbol.currentTarget.value !== ''
+        ? nameOutputEl.textContent = symbol.currentTarget.value
+        : nameOutputEl.textContent = 'Anonymous'))
+  
