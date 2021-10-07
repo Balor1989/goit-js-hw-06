5f5be9d240aacb46cf432be515aee854c7ef0c9a
Evgeniy Grishko <evgeniygrish@gmail.com>
2021-10-07 16:43:03 +0300

    Добавляет решение 8-й задачи

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const formToLogin = document.querySelector('.login-form');
+const userInformation = {}
+
+// Функция собирает данные в переменную 'userInformation' и перезагружает форму при нажатии кнопки 'Login' 
+const onSubmitLoginBtn = event => {
+    
+    event.preventDefault();
+  
+    const {
+    elements: { password, email }
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert('Внимание! Все поля должны быть заполнены!')
+  }
+    // Сбор данных в переменную 'userInformation'
+    userInformation.password = password.value;
+    userInformation.email = email.value
+    formToLogin.reset()
+}
+ console.log(userInformation)
+
+formToLogin.addEventListener('submit', onSubmitLoginBtn)
+
 
