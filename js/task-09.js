0f63d78872ce86683645f61ceb003c48715f6441
Evgeniy Grishko <evgeniygrish@gmail.com>
2021-10-07 09:51:47 +0300

    Выполняет рефакторинг кода в 9-й задаче. Исправляет баг с некорректным отбражением цвета
    Исправляет ошибку несовпадения цвета фона и отображения названия цвета

--- js/task-09.js
+++ js/task-09.js
@@ -1,11 +1,21 @@
+// Выполняет привязку ссылок к переменным
 const styleOfBody = document.querySelector('body')
 const changeColorBtn = document.querySelector('.change-color')
 const backgroundColorValue = document.querySelector('.color')
 
-changeColorBtn.addEventListener('click', getRandomHexColor)
+// Функция генерирует случайный цвет и передает его значения переменным
+const getRandomHexColor = (() => {
+  let randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  backgroundColorValue.textContent = randomColor;
+  styleOfBody.style.backgroundColor = randomColor;
+});
+// Запускает выполнение функции по нажатию на кнопку 'Change color'
+changeColorBtn.addEventListener('click', getRandomHexColor);
 
-function getRandomHexColor() {
-  backgroundColorValue.textContent = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-  styleOfBody.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+
+
+
+
+
+ // backgroundColorValue.textContent = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  // styleOfBody.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
